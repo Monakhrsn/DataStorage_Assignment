f4c4456af1c9914392d0128bf1586489d2be689b
Mona Khorasani <monakhrsn@gmail.com>
2025-01-28 13:37:08 +0100

    Develop Create,Read and update in customerEntity

--- Data/Repositories/CustomerRepository.cs
+++ Data/Repositories/CustomerRepository.cs
@@ -1,4 +1,6 @@
+using System.Linq.Expressions;
 using Data.Contexts;
+using Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repositories;
@@ -8,21 +10,56 @@ public class CustomerRepository(DataContext context)
     private readonly DataContext _context = context;
     
     // CREATE
+    public async Task<bool> AddAsync(CustomerEntity entity)
+    {
+        try
+        {
+            _context.Customers.Add(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch
+        {
+           return false;
+        }
+    }
     
     // READ
+    public async Task<IEnumerable<CustomerEntity>> GetAllAsync()
+    {
+        return await _context.Customers.ToListAsync();
+    }
+
+    public async Task<CustomerEntity?> GetAsync(Expression<Func<CustomerEntity, bool>> expression)
+    {
+        var entity = await _context.Customers.FirstOrDefaultAsync(expression);
+        return entity;
+    }
     
     // UPDATE
-        
+    public async Task<bool> UpdateAsync(CustomerEntity updatedEntity)
+    {
+        try
+        {
+            var entity = await _context.Customers.FindAsync(updatedEntity);
+            if (entity == null) return false;
+            _context.Entry(entity).CurrentValues.SetValues(updatedEntity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    
     // DELETE
     public async Task<bool> DeleteAsync(int id)
     {
         var entity = await _context.Customers.FirstOrDefaultAsync(x => x.Id == id);
-        if (entity != null)
-        {
-            _context.Customers.Remove(entity);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-        return false;
+        if (entity == null) return false;
+        _context.Customers.Remove(entity);
+        await _context.SaveChangesAsync();
+        return true;
     }
 }
