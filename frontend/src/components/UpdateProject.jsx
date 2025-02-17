e5c439f78ff76b0bd7cc5dc7d8230db79ce18a97
Mona Khorasani <monakhrsn@gmail.com>
2025-02-17 16:52:10 +0100

    Clean up, Develop more Crreate project, stylin, Nav styling, add buttons and developing the base of update project

--- frontend/src/components/UpdateProject.jsx
+++ frontend/src/components/UpdateProject.jsx
@@ -1,13 +1,13 @@
-import { Container, DropdownButton } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Row from "react-bootstrap/Row";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 
-const CreateProject = () => {
+const UpdateProject = () => {
   const [validated, setValidated] = useState(false);
   const [status, setStatus] = useState("Choose the status of the project");
   const [product, setProduct] = useState("Choose the type of the product");
@@ -24,7 +24,7 @@ const CreateProject = () => {
   return (
     <Container className="bg-light text-dark">
       <Col className="p-4">
-        <h4>PROJECT 218 - CREATE NEW</h4>
+        <h4>PROJECT 218 - EDIT</h4>
         <Row className="mb-3 mt-3">
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Row className="mb-3 border-top">
@@ -58,15 +58,22 @@ const CreateProject = () => {
               </Form.Group>
               <Form.Group as={Col} md="4" controlId="validationCustom04">
                 <Form.Label>Status</Form.Label>
-
-                <Dropdown onSelect={(eventKey) => setStatus(eventKey)} aria-label="Status" required>
+                <Dropdown
+                  onSelect={(eventKey) => setStatus(eventKey)}
+                  aria-label="Status"
+                  required
+                >
                   <Dropdown.Toggle variant="light" className="bg-white border">
                     {status}
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
-                    <Dropdown.Item eventKey="Not starteddd">Not starteddd</Dropdown.Item>
+                    <Dropdown.Item eventKey="Not starteddd">
+                      Not starteddd
+                    </Dropdown.Item>
                     <Dropdown.Item eventKey="Ongoingg">Ongoingg</Dropdown.Item>
-                    <Dropdown.Item eventKey="Finisheddd">Finisheddd</Dropdown.Item>
+                    <Dropdown.Item eventKey="Finisheddd">
+                      Finisheddd
+                    </Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
               </Form.Group>
@@ -110,15 +117,21 @@ const CreateProject = () => {
               </Form.Group>
             </Row>
             <h5>Selected Products</h5>
-            <Dropdown onSelect={(eventKey) => setProduct(eventKey)} aria-label="Product" required>
-                  <Dropdown.Toggle variant="light" className="bg-white border">
-                    {product}
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item eventKey="Educationnn">Educationnn</Dropdown.Item>
-                    <Dropdown.Item eventKey="Consultant">Consultant</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
+            <Dropdown
+              onSelect={(eventKey) => setProduct(eventKey)}
+              aria-label="Product"
+              required
+            >
+              <Dropdown.Toggle variant="light" className="bg-white border">
+                {product}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item eventKey="Educationnn">
+                  Educationnn
+                </Dropdown.Item>
+                <Dropdown.Item eventKey="Consultant">Consultant</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
             <h5>Customer</h5>
             <Row className="mb-3 border-top">
               <Form.Group as={Col} md="6" controlId="validationCustom03">
@@ -129,12 +142,11 @@ const CreateProject = () => {
                 </Form.Control.Feedback>
               </Form.Group>
             </Row>
-
             <Col className="d-flex justify-content-between">
               <Button variant="outline-warning" className="rounded-pill px-3">
                 Edit
               </Button>
-              <div className="d-flex gap-2">
+              <Stack direction="horizental" className="gap-2">
                 <Button variant="secondary" className="rounded-pill px-3">
                   Cancle
                 </Button>
@@ -145,7 +157,7 @@ const CreateProject = () => {
                 >
                   Save
                 </Button>
-              </div>
+              </Stack>
             </Col>
           </Form>
         </Row>
--- frontend/src/components/header/Nav.jsx
+++ frontend/src/components/header/Nav.jsx
@@ -4,6 +4,7 @@ import {
   Col,
   Container,
   Nav as BootstrapNav,
+  Button,
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
@@ -22,9 +23,17 @@ const Nav = () => {
           <span>Hans Mattin AB</span>
         </Navbar.Brand>
 
-        <div className="d-flex justify-content-between gap-3">
-          <BootstrapNav.Link as={Link} to="/projects">Projects</BootstrapNav.Link>
-          <BootstrapNav.Link as={Link} to="/create-project">Create Project</BootstrapNav.Link>
+        <div className="d-flex justify-content-between gap-2">
+          <Button variant="secondary" className="rounded-pill px-3">
+            <BootstrapNav.Link as={Link} to="/create-project">
+              Create Project
+            </BootstrapNav.Link>
+          </Button>
+          <Button variant="warning" className="rounded-pill px-3">
+            <BootstrapNav.Link as={Link} to="/projects">
+              Show Projects
+            </BootstrapNav.Link>
+          </Button>
         </div>
       </Container>
     </Navbar>
